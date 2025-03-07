e914f8facdf36fe7e01e11ac25357a2e9a33f86b
ozysouza <ozysouzaa@gmail.com>
2025-03-06 23:25:23 -0500

    Add functionality to login

--- marketplace/core/urls.py
+++ marketplace/core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+from .forms import LoginForm
 
 
 app_name = 'core'
@@ -7,5 +9,7 @@ app_name = 'core'
 urlpatterns = [
     path("", views.index, name="index"),
     path('contact/', views.contact, name='contact'),
-    path('signup/', views.signUp, name='signup')
+    path('signup/', views.signUp, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html',
+         authentication_form=LoginForm), name='login')
 ]
