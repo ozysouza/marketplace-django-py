5292ad5aac98fd1ddf62646eaf31562a817af5c9
ozysouza <ozysouzaa@gmail.com>
2025-03-09 01:53:37 -0500

    Create function to edit item

--- marketplace/item/views.py
+++ marketplace/item/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 
 def detail(request, pk):
@@ -22,6 +22,26 @@ def delete(request, pk):
     item.delete()
 
     return redirect('dashboard:index')
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit item'
+    })
 
 
 @login_required
